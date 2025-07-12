80f5bc91fcf03e4cb34310bdf3f03ec52bfc6035
AngelAbc <AngelAbc@AngelAbc.com>
2025-07-12 15:39:04 +0200

    Load game schema

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { logDebug } from '@utility/logMessage.ts'
+import { loadJsonResource } from '@utility/loadJsonResource.ts'
+import { gameSchema, type Game } from '@types/data/game.ts'
 
 logDebug('Application starting...')
+
+const game: Game = await loadJsonResource<Game>('./game.json', gameSchema)
+logDebug('Game loaded: {0}', game)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

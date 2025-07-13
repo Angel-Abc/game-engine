ecf6f0dc74c843d398d9d743fe592c15fb720c0f
AngelAbc <AngelAbc@AngelAbc.com>
2025-07-13 15:40:57 +0200

    Added app folder, restructured the data folder

---
+++ src/app/game.tsx
@@ -0,0 +1,9 @@
+interface GameProps {}
+
+const Game: React.FC<GameProps> = (): React.JSX.Element => {
+    return (
+        <div>TODO</div>
+    )
+}
+
+export default Game
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { logDebug } from '@utility/logMessage.ts'
 import { loadJsonResource } from '@utility/loadJsonResource.ts'
-import { gameSchema, type Game } from '@data/game'
+import { gameSchema, type Game } from '@data/game/game'
+import App from './app/game.tsx'
 
 logDebug('Application starting...')
 
 const game: Game = await loadJsonResource<Game>('/data/game.json', gameSchema)
 logDebug('Game loaded: {0}', game)
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.body)
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+

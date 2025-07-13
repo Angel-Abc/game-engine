edbf828241930e7ecd1c243a16d0b687200ef146
AngelAbc <AngelAbc@AngelAbc.com>
2025-07-14 00:38:07 +0200

    Added GameEngine state

--- src/engine/gameEngine.ts
+++ src/engine/gameEngine.ts
@@ -1,16 +1,46 @@
 import type { Game } from '@data/load/game'
-import type { IGameEngine } from './type'
-import { logInfo } from '@utility/logMessage'
+import { GameEngineState, type IGameEngine } from './type'
+import { fatalError, logDebug, logInfo } from '@utility/logMessage'
+import { TrackedValue, type ITrackedValue } from '@utility/trackedState'
+
+let gameEngine: GameEngine | null = null;
+export function getGameEngine(): IGameEngine {
+    if (gameEngine === null) {
+        fatalError('Game engine is not initialized')
+    }  
+    return gameEngine
+}
+
+function setGameEngine(engine: GameEngine): void {
+    if (gameEngine !== null) {
+        fatalError('Game engine is already initialized')
+    }
+    gameEngine = engine
+}
 
 export class GameEngine implements IGameEngine {
     private game: Game
+    private _state: ITrackedValue<GameEngineState>
 
     constructor(game: Game) {
         this.game = game
-        logInfo("Game engine initialized with game: {0}", this.game.title)
+        logInfo('Game engine initialized with game: {0}', this.game.title)
+        this._state = new TrackedValue<GameEngineState>(
+            'GameEngine.State',
+            GameEngineState.init,
+            (newValue, oldValue) => {
+                logDebug('Game engine state changed from {0} to {1}', oldValue, newValue)
+            }
+        )
+        setGameEngine(this)
     }
 
     start(): void {
-        logInfo("Game engine started");
+        logInfo('Game engine started')
     }
+
+    get State(): ITrackedValue<GameEngineState> {
+        return this._state
+    }
+
 }
--- src/engine/type.ts
+++ src/engine/type.ts
@@ -1,3 +1,14 @@
+import type { ITrackedValue } from '@utility/trackedState'
+
+export const GameEngineState = {
+    init: 0,
+    loading: 1,
+    running: 2
+} as const
+export type GameEngineState = typeof GameEngineState[keyof typeof GameEngineState]
+
 export interface IGameEngine {
     start(): void
+    get State(): ITrackedValue<GameEngineState>
 }
+

1b676adf1bebe0270a6d18b3a42e60a7b7ece298
AngelAbc <AngelAbc@AngelAbc.com>
2025-07-13 15:42:50 +0200

    More restructure data folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { logDebug } from '@utility/logMessage.ts'
 import { loadJsonResource } from '@utility/loadJsonResource.ts'
-import { gameSchema, type Game } from '@data/game/game'
+import { gameSchema, type Game } from '@data/load/game'
 import App from './app/game.tsx'
 
 logDebug('Application starting...')

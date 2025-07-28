fbd31c440186d165736b77df0298f2f7f4709de7
AngelAbc <AngelAbc@AngelAbc.com>
2025-07-28 19:40:25 +0200

    App now renders content depending on the game engine state

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,19 @@
-import { useEffect } from 'react'
-import { logInfo } from '@utils/logMessage'
+import { logDebug } from '@utils/logMessage'
+import { useSyncExternalStore } from 'react'
+import { GameEngineState, getGameEngine } from '../engine/gameEngine'
 
-export default function App() {
-  useEffect(() => {
-    logInfo('App mounted')
-  }, [])
-  return <div>Game engine initialized.</div>
+export const App: React.FC = (): React.JSX.Element => {
+    const engine = getGameEngine()
+    const engineState = useSyncExternalStore(engine.State.subscribe.bind(engine.State), () => engine.State.value)
+
+    logDebug('Game component rendered with engine state: {0}', engineState)
+
+    switch(engineState) {
+      case GameEngineState.init:
+        return (<div>Initializing game engine ...</div>)
+      case GameEngineState.loading:
+        return (<div>Loading game data ...</div>)
+      case GameEngineState.running:
+        return (<div>TODO</div>)
+    }
 }

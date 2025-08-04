c6373b5e9f6cf12058936753922e80465d663911
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-04 18:26:49 +0200

    Maintain stable keys for editor lists

--- src/editor/components/TileList.tsx
+++ src/editor/components/TileList.tsx
@@ -1,5 +1,6 @@
 import type React from 'react'
 import type { EditableMapActions } from './useEditableList'
+import { useMapKeys } from './useMapKeys'
 
 interface TileListProps extends EditableMapActions {
   tiles: Record<string, string>
@@ -11,29 +12,45 @@ export const TileList: React.FC<TileListProps> = ({
   updateItem,
   addItem,
   removeItem,
-}) => (
-  <section className="editor-section editor-list">
-    <h2>Tiles</h2>
-    {Object.entries(tiles).map(([id, path]) => (
-      <fieldset key={id} className="editor-list-item">
-        <input
-          type="text"
-          value={id}
-          onChange={(e) => updateId(id, e.target.value)}
-        />
-        <input
-          type="text"
-          value={path}
-          onChange={(e) => updateItem(id, e.target.value)}
-        />
-        <button type="button" onClick={() => removeItem(id)}>
-          Remove
-        </button>
-      </fieldset>
-    ))}
-    <button type="button" onClick={addItem}>
-      Add Tile
-    </button>
-  </section>
-)
+}) => {
+  const { getKey, updateId: updateKey, removeId } = useMapKeys(
+    Object.keys(tiles),
+  )
+
+  const handleUpdateId = (oldId: string, newId: string) => {
+    updateKey(oldId, newId)
+    updateId(oldId, newId)
+  }
+
+  const handleRemove = (id: string) => {
+    removeId(id)
+    removeItem(id)
+  }
+
+  return (
+    <section className="editor-section editor-list">
+      <h2>Tiles</h2>
+      {Object.entries(tiles).map(([id, path]) => (
+        <fieldset key={getKey(id)} className="editor-list-item">
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => handleUpdateId(id, e.target.value)}
+          />
+          <input
+            type="text"
+            value={path}
+            onChange={(e) => updateItem(id, e.target.value)}
+          />
+          <button type="button" onClick={() => handleRemove(id)}>
+            Remove
+          </button>
+        </fieldset>
+      ))}
+      <button type="button" onClick={addItem}>
+        Add Tile
+      </button>
+    </section>
+  )
+}
 

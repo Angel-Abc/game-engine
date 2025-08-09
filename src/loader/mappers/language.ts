b894f2e90c52297788a27f1a0a30b9470d96d939
AngelAbc <AngelAbc@AngelAbc.com>
2025-08-09 16:56:30 +0200

    Added some more translations. Add the option to create multiline translations that are concatinated by a cr

--- src/loader/mappers/language.ts
+++ src/loader/mappers/language.ts
@@ -4,6 +4,11 @@ import { type Language } from '@loader/schema/language'
 export function mapLanguage(language: Language): LanguageData {
     return {
         id: language.id,
-        translations: { ...language.translations }
+        translations: Object.fromEntries(
+            Object.entries(language.translations).map(([key, value]) => [
+                key,
+                Array.isArray(value) ? value.join("\n ") : value
+            ])
+        )
     }
 }
--- src/loader/schema/language.ts
+++ src/loader/schema/language.ts
@@ -2,6 +2,6 @@ import { z } from 'zod'
 
 export const languageSchema = z.object({
     id: z.string(),
-    translations: z.record(z.string(), z.string())
+    translations: z.record(z.string(), z.union([z.string(), z.array(z.string())]))
 })
 export type Language = z.infer<typeof languageSchema>

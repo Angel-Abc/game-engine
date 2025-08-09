e54940fe111d98936af0dc74e0f441b06359c5df
AngelAbc <AngelAbc@AngelAbc.com>
2025-08-09 19:49:17 +0200

    Write the dialog action to the output log

--- src/engine/actions/endDialogActionHandler.ts
+++ src/engine/actions/endDialogActionHandler.ts
@@ -3,13 +3,22 @@ import type { IGameEngine } from '@engine/core/gameEngine'
 import type { Message } from '@utils/types'
 import type { EndDialogAction } from '@loader/data/dialog'
 import { ADD_LINE_TO_OUTPUT_LOG } from '@engine/messages/messages'
+import type { InputItem } from '@engine/input/inputSourceTracker'
 
 export class EndDialogActionHandler implements IActionHandler<EndDialogAction> {
     readonly type = 'end-dialog' as const
 
-    handle(engine: IGameEngine, action: EndDialogAction, _message?: Message, _data?: unknown): void {
+    handle(engine: IGameEngine, action: EndDialogAction, _message?: Message, data?: unknown): void {
         void _message
-        void _data
+        if (data && typeof data === 'object' && 'input' in data) {
+            const inputItem = data as InputItem
+            if (inputItem && inputItem.input.description) {
+                engine.MessageBus.postMessage({
+                    message: ADD_LINE_TO_OUTPUT_LOG,
+                    payload: `<p>&gt; ${inputItem.input.description}</p>`,
+                })
+            }
+        }
         if (action.message) {
             engine.MessageBus.postMessage({
                 message: ADD_LINE_TO_OUTPUT_LOG,
